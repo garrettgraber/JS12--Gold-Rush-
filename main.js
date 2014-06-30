b7c34cce24355a7927327283e2733d42b51766b2
garrettgraber <garrett_graber@yahoo.com>
2014-06-30 15:13:46 -0600

    JS12 gold rush, part I done

--- main.js
+++ main.js
@@ -7,8 +7,11 @@ $(document).on('ready', function() {
   	var xValuePercent = (xValue/docWidth) * 100;
   	var yValuePercent = (yValue/docHeight) * 100;
   	console.log("pageX: " + event.pageX + ", pageY: " + event.pageY );
-  	console.log("height: " + $(document).height() + " width: " + $(document).width());
+  	console.log(" width: " + docWidth + "height: " + docHeight);
   	if ((89.7 >= xValuePercent) && (xValuePercent >= 9.8) && (91.8 >= yValuePercent) && (yValuePercent >= 5.78) ) {
+  		
+  		xValuePercent = xValuePercent - 0.8;
+  		yValuePercent = yValuePercent - 0.7;
   		var JqueryNew = createMarker(xValuePercent, yValuePercent);
   		JqueryNew.appendTo($('#main'));
   		console.log('it worked');
@@ -20,17 +23,25 @@ $(document).on('ready', function() {
   });
 
   $('#main-map').mousemove(function () {
+  	var xValue = event.pageX;
+  	var yValue = event.pageY;
+  	var docHeight = $(document).height();
+  	var docWidth = $(document).width();
+  	var xValuePercent = (xValue/docWidth) * 100;
+  	var yValuePercent = (yValue/docHeight) * 100;
   	console.log("pageX: " + event.pageX + ", pageY: " + event.pageY );
+  	console.log("x-percentage: " + xValuePercent + ", y-percentage: " + yValuePercent);
   });
 
   var createMarker = function(xVal, yVal) {
-  	var imgSource = 'kitty.jpg';
+
+  	var imgSource = 'thumbtack.png';
   	var jQueryObjContainer = $('<div>').addClass('gold');
   	var JqueryObjImage = $('<img>').attr('src', imgSource);
   	JqueryObjImage.addClass('kitty-pic');
   	JqueryObjImage.appendTo(jQueryObjContainer);
-  	jQueryObjContainer.css('left', xVal);
-  	jQueryObjContainer.css('top', yVal);
+  	jQueryObjContainer.css('left', xVal + "%");
+  	jQueryObjContainer.css('top', yVal + "%");
   	return jQueryObjContainer;
   };
 
