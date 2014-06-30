6fbcd72c9c4dc0de675436bc3de0414e0507758e
garrettgraber <garrett_graber@yahoo.com>
2014-06-30 16:38:38 -0600

    JS12 gold rush, Bonus I almost complete, textarea part not working

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
 $(document).on('ready', function() {
+
+var noteTemp;
+
+  var createMarker = function(xVal, yVal) {
+
+  	var imgSource = 'thumbtack.png';
+  	var jQueryObjContainer = $('<div>').addClass('gold');
+  	var JqueryObjImage = $('<img>').attr('src', imgSource);
+  	JqueryObjImage.addClass('kitty-pic');
+  	JqueryObjImage.appendTo(jQueryObjContainer);
+  	jQueryObjContainer.css('left', xVal + "%");
+  	jQueryObjContainer.css('top', yVal + "%");
+  	return jQueryObjContainer;
+  };
+
+   var noteCreater = function(note){
+  	var jQueryObject = $("<div>").addClass('textInfo');
+  	jQueryObject.text(note);
+  	return jQueryObject;
+  };
+
+  var textInputFunc = function(xVal, yVal) {
+  	console.log('textInputFunc is firing');
+  	var jQueryTextInput = $('<textarea>');
+  	jQueryTextInput.addClass('.textInput');
+  	jQueryTextInput.css('left', xVal + "%");
+  	jQueryTextInput.css('top', yVal + "%");
+  	return jQueryTextInput;
+  };
+
   $('#main-map').click(function () {
   	var xValue = event.pageX;
   	var yValue = event.pageY;
@@ -13,13 +43,25 @@ $(document).on('ready', function() {
   		xValuePercent = xValuePercent - 0.8;
   		yValuePercent = yValuePercent - 0.7;
   		var JqueryNew = createMarker(xValuePercent, yValuePercent);
-  		JqueryNew.appendTo($('#main'));
+  		var note = prompt('Please enter a note');
+  		/*
+  		var jQueryEnter = textInputFunc(xValuePercent, yValuePercent);
+  		jQueryEnter.appendTo(JqueryNew);
+
+
+  		var note = jQueryEnter.text();
+
+  		*/  		
+
+		var jQueryNote = noteCreater(note);
+		jQueryNote.hide();
+		jQueryNote.appendTo(JqueryNew);
+		JqueryNew.appendTo($('#main'));
   		console.log('it worked');
   	}
   	else {
   		console.log('yValue bad');
   	}
-
   });
 
   $('#main-map').mousemove(function () {
@@ -33,22 +75,21 @@ $(document).on('ready', function() {
   	console.log("x-percentage: " + xValuePercent + ", y-percentage: " + yValuePercent);
   });
 
-  $(document).on('click', '.gold', function() {
+  $(document).on('click', '.gold img', function() {
   	console.log('remove funtion is firing');
-  	$(this).remove();
+  	$(this).parent().remove();
   });
 
-  var createMarker = function(xVal, yVal) {
+  $(document).on('mouseenter', '.gold', function(){
+  	$(this).find('.textInfo').show();
+  });
 
-  	var imgSource = 'thumbtack.png';
-  	var jQueryObjContainer = $('<div>').addClass('gold');
-  	var JqueryObjImage = $('<img>').attr('src', imgSource);
-  	JqueryObjImage.addClass('kitty-pic');
-  	JqueryObjImage.appendTo(jQueryObjContainer);
-  	jQueryObjContainer.css('left', xVal + "%");
-  	jQueryObjContainer.css('top', yVal + "%");
-  	return jQueryObjContainer;
-  };
+  $(document).on('mouseleave', '.gold', function(){
+  	$(this).find('.textInfo').hide();
+  });
 
-  window.foo = createMarker;
+  $(document).on('keypress', '.textInfo', function(){
+  	var noteTemp = $(this).text();
+  });
+  
 });
